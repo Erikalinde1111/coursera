337fd563322f02694e2aac5aa586864aec56cff4
Erika Linde <erika.j.linde@gmail.com>
2017-04-03 22:39:02 +0200

    n

--- site/script.js
+++ site/script.js
@@ -1,8 +1,8 @@
 window.onload = function (){
 	sayHello();
-	console.log(document.getElemenById("name"));
+	
 }
 function sayHello(){
 	console.log("HEJ");
-	
+	console.log(document.getElemenById("name"));
 };
